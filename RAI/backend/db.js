8aa7ff99d1da1729541b2b6708ce8f91333c6ddf
Primoz Levstik <primoz.levstik@gmail.com>
2026-05-11 21:09:28 +0200

    povezava z bazo (db.js) in skripta za polnjenje testnih podatkov (seed.js)

---
+++ RAI/backend/db.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+// pool povezav
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+// ob uspesni povezavi javi
+pool.on('connect', () => {
+  console.log('Zagon je bil uspesen.');
+});
+
+// ob napaki javi
+pool.on('error', (err) => {
+  console.error('Napaka pri povezavah:', err);
+});
+
+// query za v drugih files
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
