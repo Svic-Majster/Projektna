9ef63cbf0567905f3ede5292e516eaa4fc476c61
Primoz Levstik <primoz.levstik@gmail.com>
2026-05-20 19:37:12 +0200

    Popravljena funkcija pridobi_poti_map za train/test

--- ORV/procesiraj_slike.py
+++ ORV/procesiraj_slike.py
@@ -2,12 +2,16 @@ import cv2
 import os
 import glob
 
-# pridobi pot originalnih slik in preveri ce obstaja pot za procesirane. (ce ne jo nardi)
+# pridobi pot originalnih slik in preveri ce obstaja pot za procesirane. (ce ne jo nardi) (train za treniranje modela test za testiranje)
 def pridobi_poti_map(uporabnik_id, surova_baza="data/raw", predelana_baza="data/processed"):
     izvorna_pot = os.path.join(surova_baza, str(uporabnik_id))
     ciljna_pot = os.path.join(predelana_baza, str(uporabnik_id))
     
-    if not os.path.exists(ciljna_pot) and os.path.exists(izvorna_pot):
-        os.makedirs(ciljna_pot)
+    pot_train = os.path.join(ciljna_pot, "train")
+    pot_test = os.path.join(ciljna_pot, "test")
+    
+    if os.path.exists(izvorna_pot):
+        os.makedirs(pot_train, exist_ok=True)
+        os.makedirs(pot_test, exist_ok=True)
         
-    return izvorna_pot, ciljna_pot
+    return izvorna_pot, pot_train, pot_test
