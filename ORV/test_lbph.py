725ed4232395b92de0dacd68e9e7d1cac678f7d0
temacni7 <mark.markez@student.um.si>
2026-05-21 13:15:18 +0200

    Dodana verification logika za LBPH model

--- ORV/test_lbph.py
+++ ORV/test_lbph.py
@@ -4,6 +4,9 @@ import os
 MODEL_PATH = "lbph_model.yml"
 TEST_PATH = "data/processed/1/test"
 
+# začasni prag za potrditev uporabnika
+THRESHOLD = 50
+
 # naloži LBPH model
 model = cv2.face.LBPHFaceRecognizer_create()
 model.read(MODEL_PATH)
@@ -21,7 +24,14 @@ for filename in os.listdir(TEST_PATH):
     # napoved uporabnika
     predicted_label, confidence = model.predict(image)
 
+    # preveri ujemanje
+    if predicted_label == 1 and confidence < THRESHOLD:
+        status = "POTRJEN"
+    else:
+        status = "ZAVRNJEN"
+
     print(f"Slika: {filename}")
     print(f"Napovedan uporabnik: {predicted_label}")
-    print(f"Confidence: {confidence}")
+    print(f"Confidence: {confidence:.2f}")
+    print(f"Status: {status}")
     print("----------------------")
