3b3501dfa93328a4669a1ae9dd0d6a8e0df50e95
Primoz Levstik <primoz.levstik@gmail.com>
2026-05-22 00:50:54 +0200

    dodano preverjanje ivedb

--- SA/tests/javascript/dbSeed.test.js
+++ SA/tests/javascript/dbSeed.test.js
@@ -1,3 +1,4 @@
+module.paths.push('../../../RAI/backend/node_modules');
 const bcrypt = require('bcrypt');
 
 // ustvarimo mock db za test
@@ -32,6 +33,21 @@ it('mora uspesno pobrisati stare in vstaviti 3 nove testne uporabnike', async ()
 
         // cakamo next tick
         await new Promise(process.nextTick);
+
+        // preveri ce se je izvedel DELETE
+        expect(mockQuery).toHaveBeenNthCalledWith(1, 
+            'DELETE FROM uporabniki WHERE email LIKE $1', 
+            ['%@test.si']
+        );
+
+        // preverimo ce so se izvedli vsi klici
+        expect(mockQuery).toHaveBeenCalledTimes(4);
+
+        // preverimo ce je bil dodan admin
+        expect(mockQuery).toHaveBeenLastCalledWith(
+            'INSERT INTO uporabniki (ime, priimek, username, email, geslo) VALUES ($1, $2, $3, $4, $5)',
+            expect.arrayContaining(['Švic', 'Mojster', 'svic_mojster', 'admin@test.si'])
+        );
 
         // preveri ce se je skripta koncala brez napak
         expect(exitSpy).toHaveBeenCalledWith(0);
