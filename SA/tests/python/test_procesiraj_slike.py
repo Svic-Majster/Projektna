b5224b205c0f1706b7d8cf9594ee1974cfd188db
Primoz Levstik <primoz.levstik@gmail.com>
2026-05-21 20:18:58 +0200

    funkcija za testiranje pridobi_poti_map#2

--- SA/tests/python/test_procesiraj_slike.py
+++ SA/tests/python/test_procesiraj_slike.py
@@ -35,3 +35,23 @@ def test_pridobi_poti_map_ko_izvor_obstaja():
     if os.path.exists("data"):
         shutil.rmtree(test_raw, ignore_errors=True)
         shutil.rmtree(test_processed, ignore_errors=True)
+
+# test ko mapa ne obstaja
+def test_pridobi_poti_map_ko_izvor_ne_obstaja():
+    test_raw = "data/test_raw_poti_ne_obstaja"
+    test_processed = "data/test_processed_poti_ne_obstaja"
+    uporabnik_id = 2
+    
+    # ce obstaja jo zbrisemo
+    if os.path.exists(test_raw):
+        shutil.rmtree(test_raw)
+        
+    # poklicemo funkcijo
+    izv, train, test = pridobi_poti_map(uporabnik_id, surova_baza=test_raw, predelana_baza=test_processed)
+    
+    # se vedno more vrnit path
+    assert izv == os.path.join(test_raw, str(uporabnik_id))
+    
+    # mapi nesmeta bit ker ni ivora
+    assert not os.path.exists(train)
+    assert not os.path.exists(test)
