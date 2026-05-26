2da3f76c9488934b63e10f80ad72ca76ccfab462
temacni7 <mark.markez@student.um.si>
2026-05-26 16:39:55 +0200

    funkcija za testiranje treniranja LBPH modela

--- SA/tests/python/test_lbph_model.py
+++ SA/tests/python/test_lbph_model.py
@@ -1,7 +1,21 @@
 import cv2
+import numpy as np
 
 
 def test_lbph_model_creation():
     model = cv2.face.LBPHFaceRecognizer_create()
+
+    assert model is not None
+
+def test_lbph_model_training():
+    model = cv2.face.LBPHFaceRecognizer_create()
+
+    image1 = np.zeros((100, 100), dtype=np.uint8)
+    image2 = np.ones((100, 100), dtype=np.uint8) * 255
+
+    images = [image1, image2]
+    labels = np.array([1, 1])
+
+    model.train(images, labels)
 
     assert model is not None
