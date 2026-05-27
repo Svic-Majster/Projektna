8c0d3da27a203bffa8bae78cd4675101403c556c
Primoz Levstik <primoz.levstik@gmail.com>
2026-05-27 18:06:00 +0200

    sprememba da posilja data in bulk

--- RAI/scraper/scraper.py
+++ RAI/scraper/scraper.py
@@ -2,10 +2,10 @@ import requests
 import xml.etree.ElementTree as ET
 import time
 
-API_URL = "http://localhost:3000/api/external/data"
+API_URL = "http://localhost:3000/api/zunanji-viri/bulk"
 
 def scrape_arso_basics():
-    print("\nzacetek scraperja")
+    print("\n--- Zacetek scraperja ---")
     arso_url = "https://meteo.arso.gov.si/uploads/probase/www/observ/surface/text/sl/observation_si_latest.xml"
     
     try:
@@ -15,10 +15,9 @@ def scrape_arso_basics():
             return
 
         root = ET.fromstring(response.content.decode('utf-8'))
-        
         postaje = root.findall('metData')
         
-        uspesno_shranjenih = 0
+        vsi_podatki = []
         
         for met_data in postaje:
             kraj_ime = met_data.find('domain_longTitle').text if met_data.find('domain_longTitle') is not None else None
@@ -47,20 +46,20 @@ def scrape_arso_basics():
                 "kraj": kraj_ime
             }
             
-            try:
-                api_response = requests.post(API_URL, json=payload, timeout=5)
-                if api_response.status_code == 201:
-                    uspesno_shranjenih += 1
-                    print(f"{uspesno_shranjenih} > Uspesno posodobljeno: {kraj_ime}")
-                else:
-                    print(f"Streznik zavrnil {kraj_ime} (Status: {api_response.status_code})")
-            except requests.exceptions.RequestException as e:
-                print(f"Napaka pri posiljanju za {kraj_ime}: {e}")
-                break
-                
-            time.sleep(0.05)
+            vsi_podatki.append(payload)
             
-        print(f"\nUspesno obdelanih {uspesno_shranjenih} lokacij.")
+        if vsi_podatki:
+            print(f"Pripravljenih {len(vsi_podatki)} lokacij za pošiljanje...")
+            try:
+                api_response = requests.post(API_URL, json=vsi_podatki, timeout=15)
+                if api_response.status_code == 200:
+                    print("Uspešno posodobljeno v bazi (Bulk insert).")
+                else:
+                    print(f"Streznik zavrnil bulk zahtevo (Status: {api_response.status_code})")
+            except requests.exceptions.RequestException as e:
+                print(f"Napaka pri pošiljanju bulk podatkov: {e}")
+        else:
+            print("Ni podatkov za posodabljanje.")
 
     except Exception as e:
         print(f"Napaka v Pythonu: {e}")
