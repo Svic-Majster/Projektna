5e4e7485fdfc4a9888763f1ba522d698a561489a
temacni7 <mark.markez@student.um.si>
2026-05-27 13:20:51 +0200

    Dodan gumb za začetek treninga

--- NPO/SvicMajster/src/screens/HomeScreen.tsx
+++ NPO/SvicMajster/src/screens/HomeScreen.tsx
@@ -1,48 +1,57 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.logo}>SvicMajster</Text>
-        <Text style={styles.profile}>👤</Text>
-      </View>
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.logo}>SvicMajster</Text>
+                <Text style={styles.profile}>👤</Text>
+            </View>
 
-      <View style={styles.content}>
-        <Text style={styles.title}>Začetni zaslon</Text>
-      </View>
-    </View>
-  );
+            <View style={styles.content}>
+                <Text style={styles.title}>Začetni zaslon</Text>
+
+                <View style={styles.startButton}>
+                    <Button title="Začni trening" onPress={() => { }} />
+                </View>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#111827',
-    padding: 24,
-  },
-  header: {
-    marginTop: 32,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  logo: {
-    color: 'white',
-    fontSize: 24,
-    fontWeight: '700',
-  },
-  profile: {
-    fontSize: 28,
-  },
-  content: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    color: 'white',
-    fontSize: 22,
-    fontWeight: '600',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#111827',
+        padding: 24,
+    },
+    header: {
+        marginTop: 32,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    logo: {
+        color: 'white',
+        fontSize: 24,
+        fontWeight: '700',
+    },
+    profile: {
+        fontSize: 28,
+    },
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        color: 'white',
+        fontSize: 22,
+        fontWeight: '600',
+    },
+
+    startButton: {
+        marginTop: 24,
+        width: '80%',
+    },
 });
